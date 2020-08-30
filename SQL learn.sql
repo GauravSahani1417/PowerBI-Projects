ae5558e01a7c310e4e36f779f4ac3f2fde3ca458
Gaurav Rajesh Sahani <gauravsahani1417@gmail.com>
2020-08-30 19:57:16 +0530

    Add files via upload

---
+++ SQL learn.sql
@@ -0,0 +1,2 @@
+SELECT SUM(sales.transactions.sales_amount) FROM sales.transactions INNER JOIN sales.date ON sales.transactions.order_date=sales.date.date 
+where sales.date.year='2020' and sales.transactions.market_code='Mark012';
